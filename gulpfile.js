f66bad8d7afb3a18c7d4cb6a7b6999e3f210baac
Max Alyokhin <alexmaxvik@yandex.ru>
2019-10-14 14:23:09 +0300

    Update 14.10.19
    Настроено окружение

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,54 @@
 'use strict';
 
-const { src, dest, watch, series, parallel } = require('gulp');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
-const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
-const imageminPngquant = require('imagemin-pngquant');
-const rigger = require('gulp-rigger');
-const rimraf = require('rimraf');
-const browserSync = require('browser-sync');
-const reload = browserSync.reload;
+const { src, dest, watch, series, parallel } = require('gulp'),
+                            sass             = require('gulp-sass'), // Sass компиллятор
+                            sourcemaps       = require('gulp-sourcemaps'), // Sourcemaps
+                            autoprefixer     = require('gulp-autoprefixer'), // Автопрефиксер CSS
+                            uglify           = require('gulp-uglify'), // Минификация js
+                            imagemin         = require('gulp-imagemin'), // Компрессия изображений
+                            imageminPngquant = require('imagemin-pngquant'), // Дополнение для .png
+                            rimraf           = require('rimraf'), // Удаление файлов
+                            order            = require("gulp-order"), // Задаёт порядок конкатенации
+                            concat           = require('gulp-concat'), // Конкатенация
+                            babel            = require('gulp-babel'), // Транспайлер в ES5
+                            rigger           = require('gulp-rigger'), //Подключение шаблонов
+                            browserSync      = require('browser-sync'), // Сервер
+                            reload           = browserSync.reload;
 
 //Здесь хранятся пути до всех файлов
-var path = {
+const path = {
     //Брать исходники здесь:
     src: { 
-        html:  'src/html/**/*.html',
+        html:  'src/*.html',
         sass:  'src/sass/**/*.sass',
-        js:    'src/js/**/*.js',
+        js:    'src/js/**/*.js', 
         img:   'src/images/**/*.*',
-        fonts: 'src/fonts/**/*.*'
+        fonts: 'src/fonts/**/*.*',
+        libs:  'src/libs/**/*.js',
     },
     //За изменением каких файлов мы хотим наблюдать:
     watch: { 
-        html:  'src/html/**/*.html',
+        html:  'src/**/*.html',
+        sass:  'src/sass/**/*.sass',
         js:    'src/js/**/*.js',
-        sass: 'src/sass/**/*.sass',
         img:   'src/images/**/*.*',
-        fonts: 'src/fonts/**/*.*'
+        fonts: 'src/fonts/**/*.*',
+        libs:  'src/libs/**/*.js'
     },
     //Готовые после сборки файлы переносим сюда:
     dist: { 
         html:  'dist/',
-        css:   'dist/css/',
-        js:    'dist/js/',
-        img:   'dist/images/',
-        fonts: 'dist/fonts/'
+        css:   'dist/styles/',
+        js:    'dist/scripts/',
+        img:   'dist/img/',
+        fonts: 'dist/fonts/',
+        libs:  'dist/libs/'
     },
     clean: 'dist'
 };
 
 //Здесь настройки сервера
-var config = {
+const config = {
     server: {
         baseDir: "dist"
     },
@@ -54,7 +60,7 @@ var config = {
 };
 
 //Сборка html
-function html_build() {
+function html() {
     return src(path.src.html) //Путь до исходных файлов в src
         .pipe(rigger()) //Rigger позволяет использовать шаблоны и подключать их в документы
         .pipe(dest(path.dist.html)) //Вывод готового в dist
@@ -62,33 +68,43 @@ function html_build() {
 }
 
 //Сборка css
-function style_build() {
+function style() {
     return src(path.src.sass) //Путь до исходных файлов в src
         .pipe(sourcemaps.init()) //Инициализируем sourcemaps
         .pipe(sass({ //Параметры gulp-sass
-            sourceMap: true, //sourcemaps включены
+            sourceMap: false, //sourcemaps выключены
             errLogToConsole: true, //Пишем логи
             outputStyle: 'compressed' //Минифицируем
         }))
-        .pipe(autoprefixer()) //Добавляем вендорные префиксы, настраивается через package.json в browserlist
+        .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({
+            grid: true,
+            overrideBrowserslist: ['last 10 versions']})) //Добавляем вендорные префиксы, настраивается также через package.json в browserlist
         .pipe(sourcemaps.write()) //Прописываем sourcemaps
         .pipe(dest(path.dist.css)) //Вывод готового в dist
         .pipe(reload({stream: true})); //Обновляем сервер
 }
 
 //Сборка js
-function js_build() {
-    return src(path.src.js) //Путь до исходных файлов в src
-        .pipe(rigger()) //Rigger позволяет использовать шаблоны и подключать их в документы
-        .pipe(sourcemaps.init()) //Инициализируем sourcemaps
+function js() {
+    return src([path.src.js, path.src.libs]) //Путь до исходных файлов в src
+        // .pipe(sourcemaps.init()) //Инициализируем sourcemaps
+        .pipe(babel({ // Запускаем Babel
+			    presets: ['@babel/preset-env']
+		    }))
+        .pipe(order([ // Порядок сборки (сначала jq)
+            path.src.libs,
+            path.src.js
+        ]))
+        .pipe(concat('main.min.js'))
         .pipe(uglify()) //Минифицируем js
-        .pipe(sourcemaps.write()) //Пишем логи
+        // .pipe(sourcemaps.write()) //Пишем sourcemaps
         .pipe(dest(path.dist.js)) //Вывод готового в dist
         .pipe(reload({stream: true})); //Обновляем сервер
 }
 
 //Оптимизация изображений
-function image_build() {
+function image() {
     return src(path.src.img) //Путь до исходных файлов в src
         .pipe(imagemin({plugins: [imageminPngquant()]})) //Оптимизация изображений + плагин для png
         .pipe(dest(path.dist.img)) //Вывод готового в dist
@@ -96,7 +112,7 @@ function image_build() {
 }
 
 //Шрифты (перенос из src в dist)
-function fonts_build() {
+function fonts() {
     return src(path.src.fonts) //Вход
         .pipe(dest(path.dist.fonts)) //Выход
         .pipe(reload({stream: true})); //Обновляем сервер
@@ -112,25 +128,32 @@ function clean(cb) {
 //Удалить dist
 exports.clean = clean;
 
+//Собрать Sass
+exports.sass = style;
+
+//Собрать js
+exports.js = js;
+
 //Собрать проект
-exports.build = series(clean, html_build, style_build, js_build, image_build, fonts_build);
+exports.build = series(clean, html, style, js, image, fonts);
 
 //Запуск сервера
 exports.dev = function() {
     browserSync(config)
-    watch(path.src.html, html_build)
-    watch(path.src.sass, style_build)
-    watch(path.src.js, js_build)
-    watch(path.src.img, image_build)
-    watch(path.src.fonts, fonts_build)
+    watch(path.src.html, html)
+    watch(path.src.sass, style)
+    watch(path.src.js, js)
+    watch(path.src.img, image)
+    watch(path.src.fonts, fonts)
 }
 
 //По дефолту всё собираем и запускаем сервер
-exports.default = parallel(html_build, style_build, js_build, image_build, fonts_build, function() {
+exports.default = parallel(html, style, js, image, fonts, function() {
     browserSync(config)
-    watch(path.src.html, html_build)
-    watch(path.src.sass, style_build)
-    watch(path.src.js, js_build)
-    watch(path.src.img, image_build)
-    watch(path.src.fonts, fonts_build)
+    watch(path.src.html, html)
+    watch(path.src.sass, style)
+    watch(path.src.js, js)
+    watch(path.src.img, image)
+    watch(path.src.fonts, fonts)
 })
+
