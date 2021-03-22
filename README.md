35b94e68a00f1f1828b6fe8e368299669e3aaba9
Max Alyokhin <55048182+MaxAlyokhin@users.noreply.github.com>
2021-03-23 00:21:34 +0300

    update 22.03.21

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
-# CPD
+# Website for psychological software
 
-## Website for psychological software
- 
-Instruments: HTML5, CSS3, Flexbox, Sass, JS (jQuery), wow.js, animate.css, Gulp, VS Code, npm
- 
-## CPD — Computer Psychological Diagnostics
- 
-This software is the largest collection of psychological tests and techniques that significantly automate the work of a psychologist.
- 
-## Project implementation
- 
 ![](https://cpd-program.ru/img/design.png)
- 
-The project is a multi-page site. The bundling was assembled in Gulp, and the Rigger template engine was also used. Animation of interface elements is carried out with the help of wow.js library in a complex with animate.css.
 
-You can check the project by clicking here:
 https://cpd-program.ru
+
+Stack: HTML5, Sass, jQuery, wow.js, animate.css, Gulp
+
+## CPD — Computer Psychological Diagnostics
+
+This software is the largest collection of psychological tests and techniques that significantly automate the work of a psychologist.
+
+## Project implementation
+
+The project is a multi-page site. The bundling was assembled in Gulp, and the Rigger template engine was also used. Animation of interface elements is carried out with the help of wow.js library in a complex with animate.css.
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,29 +1,42 @@
-$( document ).ready(function(){
+$(document).ready(function () {
+  let click = 1
 
-  let click = 1;
-
-  $('.hamburger').on('click', function(){
-    
-    $(this).toggleClass('hamburger_active'); // Запускаем анимацию гамбургера
-    $('body').toggleClass('overflow-hidden'); // Скрываем скролл
+  $('.hamburger').on('click', function () {
+    $(this).toggleClass('hamburger_active') // Запускаем анимацию гамбургера
+    $('body').toggleClass('overflow-hidden') // Скрываем скролл
     if (click) {
-      $('.menu').toggleClass('active');
-      setTimeout(function () { $('.menu').toggleClass('visible')}, 100);
+      $('.menu').toggleClass('active')
+      setTimeout(function () {
+        $('.menu').toggleClass('visible')
+      }, 100)
       // По-очереди показываем пункты меню
-      setTimeout(function () { $('.index').toggleClass('translate'); }, 100);
-      setTimeout(function () { $('.methods').toggleClass('translate'); }, 200);
-      setTimeout(function () { $('.contacts').toggleClass('translate'); }, 400);
-      click = 0;
+      setTimeout(function () {
+        $('.index').toggleClass('translate')
+      }, 100)
+      setTimeout(function () {
+        $('.methods').toggleClass('translate')
+      }, 200)
+      setTimeout(function () {
+        $('.contacts').toggleClass('translate')
+      }, 400)
+      click = 0
+    } else {
+      setTimeout(function () {
+        $('.index').toggleClass('translate')
+      }, 300)
+      setTimeout(function () {
+        $('.methods').toggleClass('translate')
+      }, 200)
+      setTimeout(function () {
+        $('.contacts').toggleClass('translate')
+      }, 100)
+      setTimeout(function () {
+        $('.menu').toggleClass('visible')
+      }, 300)
+      setTimeout(function () {
+        $('.menu').toggleClass('active')
+      }, 1500)
+      click = 1
     }
-    else {
-      setTimeout(function () { $('.index').toggleClass('translate'); }, 300);
-      setTimeout(function () { $('.methods').toggleClass('translate'); }, 200);
-      setTimeout(function () { $('.contacts').toggleClass('translate'); }, 100);
-      setTimeout(function () { $('.menu').toggleClass('visible')}, 300);
-      setTimeout(function () { $('.menu').toggleClass('active')}, 1500); 
-      click = 1;	
-    }
-    
   })
-
 })
